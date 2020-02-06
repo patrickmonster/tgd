172023a3eb274a4d1d7c9f51f34ffbe10d4800b2
Soung jin Ryu <rsj11201@gmail.com>
2020-02-07 04:19:54 +0900

    Update effect.js

--- js/effect.js
+++ js/effect.js
@@ -19,7 +19,7 @@
                 }).html(options.html),
           func = function() {
             setTimeout(func,options.newOn);
-            var startPositionLeft =-500 + (Math.random() * documentWidth*2),
+            var startPositionLeft =(0-options.leftMove) + (Math.random() * documentWidth*2),
                 startOpacity = 1 + Math.random(),
                 sizeFlake = options.minSize + Math.random() * options.maxSize,
                 endPositionLeft = startPositionLeft - 100 + Math.random() * options.leftMove,
