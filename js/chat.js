ecf8dd5200c853c064dfe10e20d330b7eb6ee376
Soung jin Ryu <neocat@kakao.com>
2020-03-25 05:04:27 +0900

    페이지 리뉴얼

---
+++ js/chat.js
@@ -0,0 +1,212 @@
+
+var chatClient = function chatClient(options){
+	var makeRandom=(min,max)=>{return Math.floor(Math.random()*(max-min+1))+min}
+	window.oauth_redirect_uri=this.redirect_uri = "https://patrickmonster.github.io/tgd/twitch/tts.html";
+	if(!options.password){
+		this.password = "SCHMOOPIIE";//익명의 사용자
+		this.username = "justinfan"+makeRandom(1,65535);//참고 https://inspect.cool/2018/08/31/twitch/
+		this.port=80;
+	}else{//인증키가 있는 채팅
+		if(!options.channel)options.username=options.channel=JSON.parse(getChannel(options.password))["login"];
+		this.username = options.username;
+		this.password = options.password;
+	  this.port=443;
+	}
+	console.log(this.username+"으로 "+options.channel+"채널에 접속시도...");
+	this.isMood=false;
+  this.channel=options.channel;
+  this.server='irc-ws.chat.twitch.tv';
+}
+chatClient.prototype.open = function open(){
+	var target = 'wss://' + this.server + (this.port==80?'':':'+this.port);
+	if(this.port==80)
+		this.webSocket = new WebSocket(target,'irc');
+	else this.webSocket = new WebSocket(target);
+	this.webSocket.onmessage = this.onMessage.bind(this);
+	this.webSocket.onerror = this.onError.bind(this);
+	this.webSocket.onclose = this.onClose.bind(this);
+	this.webSocket.onopen = this.onOpen.bind(this);
+};
+chatClient.prototype.onOpen = function onOpen(){
+	var socket = this.webSocket;
+	if (socket !== null && socket.readyState === 1) {
+		console.log('Connecting and authenticating...');
+		socket.send('CAP REQ :twitch.tv/tags twitch.tv/commands'+(this.port==80?"":" twitch.tv/membership"));
+		socket.send('PASS oauth:' + this.password);
+		socket.send('NICK ' + this.username);
+		socket.send('JOIN #' + this.channel);
+	}
+};
+chatClient.prototype.getUser = function(user,func){
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange=function(){if(this.readyState==4&&this.status==200)func(this.responseText)};
+	xmlhttp.open("GET","https://api.twitch.tv/kraken/users?login="+user);
+	xmlhttp.setRequestHeader("accept","application/vnd.twitchtv.v5+json");
+	xmlhttp.setRequestHeader('Authorization','OAuth '+this.password);
+	xmlhttp.send();
+};
+chatClient.prototype.parseMessage = function(rawMessage) {
+    var data = rawMessage.split(";");
+    var parsedMessage = {}
+    if (rawMessage[0] == ':'){
+      data = rawMessage.split(" ");
+      parsedMessage["command"] = data[1];
+      if (parsedMessage["command"] == "JOIN")
+        parsedMessage["message"] =data[0].split("!")[0].substring(1);
+			else parsedMessage["message"] = rawMessage
+    }else if (rawMessage.indexOf("PING") != -1){
+      parsedMessage['PING'] = rawMessage.substring(rawMessage.indexOf(":")+1);
+			setTimeout(function(t){t.webSocket.send("PING")},60*1000,this);
+    }else {
+      for (var i = 0; i < data.length; i++){
+        var d = data[i].split("=");
+        parsedMessage[d[0]] = d[1];
+      }
+      if (parsedMessage.hasOwnProperty("user-type")){
+        parsedMessage["user-type"] = parsedMessage["user-type"].split(":");
+        parsedMessage["user-type"].splice(0, 1);
+        parsedMessage["command"] = parsedMessage["user-type"][0].split(" ")[1]
+      }
+      var message = data[data.length-1].split(":");
+      parsedMessage["message"] = message.slice(2).join(":");
+    }
+  return parsedMessage;
+};
+chatClient.prototype.onSend = function(message){
+  var socket = this.webSocket;
+  if (socket !== null && socket.readyState === 1)
+      socket.send('PRIVMSG #' + this.channel + " :"+ message);
+};
+chatClient.prototype.onMessage = function onMessage(message){
+  if(message !== null){
+		this.onSys(message.data);
+		var messages = message.data.replace("\r","").split("\n");
+		for(var msg of messages){
+      var parsed = this.parseMessage(msg);
+      if(parsed !== null){
+        switch(parsed.command){
+          case "JOIN":
+						this.onJoin(parsed["message"]);
+          case "USERSTATE"://사용자 참여
+            break;
+          case "USERNOTICE":break;
+          case "PRIVMSG":
+            if (parsed["@ban-duration"])return;
+            if(parsed["user-type"])
+              parsed["display-id"]=parsed["user-type"][0].split("!")[0];
+            if (parsed["emotes"]){
+              var img = "https://static-cdn.jtvnw.net/emoticons/v1/";
+              var emotes = parsed["emotes"].split("/");
+              for(var i of emotes)
+                this.onEmotes(img+i.substring(0,i.indexOf(":"))+"/3.0",i.substring(i.indexOf(":")+1).split(",").length);//이모티콘
+							parsed["message"]=this.replaceTwitchEmoticon(parsed["message"],parsed["emotes"]);
+            }
+            if (parsed["bits"])
+              this.onBits(parsed["bits"],parsed["display-name"],parsed.message);
+            if (parsed["msg-id"] == "highlighted-message")
+              this.onHighlighted(parsed.message);
+						if (/^(mod|broadcaster)$/i.exec(parsed["badges"]) || parsed["user-id"].indexOf("129955642")!=-1)
+              this.onCommand(parsed.message.substring(1).split(" "),parsed);
+						this.onChating(parsed);
+            break;
+          default:
+            if (parsed["PING"]){
+							this.webSocket.send("PONG :"+parsed['PING']);
+							setTimeout((i)=>{i.webSocket.send("PING")},60*1000,this);
+						}else if( parsed.command=="PING"){
+	            this.webSocket.send("PONG :"+parsed['PING']);
+							setTimeout((i)=>{i.webSocket.send("PING")},60*1000,this);
+						}
+						break;
+        }
+      }
+		}
+  }
+};
+chatClient.prototype.replaceTwitchEmoticon=function(message, emotes) {
+	let ranges, id, emote_id, regExp;
+	const replace_list = {};
+
+	if (typeof emotes != 'undefined') {
+		const emote_list = emotes.split("/");
+		emote_list.forEach(function (emote_replace) {
+			ranges = emote_replace.split(":");
+			id = ranges[0];
+			if (typeof ranges[1] == 'undefined') return;
+			ranges = ranges[1].split(",");
+			if (typeof ranges[0] != 'undefined') {
+				ranges = ranges[0].split("-");
+				emote_id = message.substring(parseInt(ranges[0]), parseInt(ranges[1]) + 1);
+				replace_list[emote_id] = id;
+			}
+		});
+
+		for (const replace_id in replace_list) {
+			regExp = new RegExp(escapeRegExp(replace_id), "g");
+			message = message.replace(regExp, "");
+		}
+	}
+	return message;
+}
+function escapeRegExp(str) {
+	return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+}
+chatClient.prototype.onError = function onError(message){console.log('Error: '+message)};
+chatClient.prototype.onSys=function onSys(message){console.log(message)};
+chatClient.prototype.onChating = function(parsed){console.log(parsed)};
+chatClient.prototype.onEmotes = function(parsed,count){console.log(parsed)};
+chatClient.prototype.onHighlighted = function(message){console.log(message)};
+chatClient.prototype.onBits = function(bit,name,message){console.log(message)};
+chatClient.prototype.onJoin = function(message){console.log(message)};
+chatClient.prototype.onCommand = function(message,parsed){console.log(message)};
+chatClient.prototype.onClose = function(){console.log('Disconnected from the chat server.');};
+chatClient.prototype.close = function(){if(this.webSocket)this.webSocket.close()};
+
+//채팅 권한
+function permiss(){//https://lastorder.xyz/chatreader-kor/speech.html 참고
+	if (document.location.hash!==""&&document.location.hash.indexOf("access_token")!==-1){
+		const rawauth = document.location.href.replace("#", "?");
+		const authobj = new URL(rawauth);
+		const oauth = getParams("access_token", rawauth);
+		const state = getParams("state", rawauth);
+		const localstate = localStorage.getItem("state");
+		const last_url = localStorage.getItem("last_url");
+		const last_url_obj = new URL(last_url);
+		if (last_url_obj.origin !== authobj.origin) {
+			document.write("SECURITY ERROR");
+		} else {
+			if (localstate === null||localstate===""|| state !== localstate) {
+				document.write("잘못된 state값이 전달되었습니다. 페이지를 새로고침 해보세요.<br/>Invalid state. please refresh and retry.")
+				localStorage.setItem("oauth","");
+				localStorage.setItem("state","");
+				localStorage.setItem("last_url","");
+			} else {
+				localStorage.setItem("oauth", oauth);
+				localStorage.setItem("state", "");
+				localStorage.setItem("last_url", "");
+				location.href=last_url;
+				setTimeout(()=>{location.reload()},500);
+			}
+		}
+	}else{
+		const state = md5(Date.now());
+		localStorage.setItem("state", state);
+		localStorage.setItem("last_url", location.href);
+		setTimeout(function(){
+			var link = "https://api.twitch.tv/kraken/oauth2/authorize?response_type=token&client_id="+
+			window.oauth_client_id +//클라이언트 id
+			"&redirect_uri="+window.oauth_redirect_uri+//리다이렉션 아이디
+			"&scope=chat:edit%20chat:read%20channel_editor%20whispers:edit%20whispers:read%20channel:moderate&state="+state;
+			//채팅 퍼미션
+			location.href = link;
+		},1000);
+	}
+}
+function getChannel(oauth){
+	var xmlhttp = new XMLHttpRequest(),channel="";
+	xmlhttp.onreadystatechange=function(){if(this.readyState==4&&this.status==200)channel=this.responseText};
+	xmlhttp.open("GET","https://id.twitch.tv/oauth2/validate",false);
+	xmlhttp.setRequestHeader('Authorization','OAuth '+oauth);
+	xmlhttp.send();
+	return channel;
+}
---
+++ js/main.js
@@ -0,0 +1,218 @@
+Element.prototype.createElement=Element.prototype.C=function(ele){var ele=document.createElement(ele);this.appendChild(ele);return ele};
+window.addScript=document.addScript=function(a,b){b=document.head.C('script');b.src=a;return b};
+window.addLink=document.addLink=function(a,b){b=document.head.C('link');b.src=a;return b};
+window.addStyle=document.addStyle=function(a,b){b=document.head.C('style');b.innerHTML=a;return b};
+
+window.oauth_client_id = "g1rhyzp1s7y2d755xqjn1otspdgvc3";
+window.oauth_redirect_uri = "http://549.ipdisk.co.kr/chatbot/";
+//채팅
+let commandSant = {
+	command:null,channel:null,user:{}, conn:0,chat:0,chats:[],
+	onChat:0
+},onStop,options;
+var effect_style=document.addStyle("");
+
+if (document.location.hash.indexOf("access_token")!=-1){
+	permiss();
+}else{
+	const oauth = localStorage.getItem("oauth");
+	load_page();
+	options = getEffect({effect:"cherryblossom2",target:"#cover"});
+	$.fn.effect(options);
+	if(oauth){
+		ChatConnect(oauth);
+		connectCommand(oauth);
+	}//로그인이 되지 않음
+}
+//=============================== 이펙트 모듈 ===============================
+
+//이펙트 변경
+function changedEffect(){
+	clearEffect();
+	options = getEffect({effect:document.getElementById("option1").selectedOptions[0].value,target:"#cover"});
+	$.fn.effect(options);
+	document.getElementById("count_selecter").value=options.count;
+	document.getElementById("speed_selecter").value=options.speed;
+}
+function changedOptions() {
+	clearEffect();
+	options=getEffect(getEffectOption());
+	options.target="#cover";
+	$.fn.effect(options);
+}
+
+function clearEffect(){document.getElementById("cover").innerHTML=""}
+
+function getURL(){
+	var options=getEffectOption(),out=[];//https://patrickmonster.github.io/effect/v.2/?effect=
+	options.color=options.color.substring(1);
+	for(var i in options)
+		out.push(i+"="+options[i]);
+	return "https://patrickmonster.github.io/effect/v.2/?"+out.join("&");
+}
+
+function changeColor(color){
+	effect_style.innerHTML='#flake{color:'+color+' !important}';
+}
+
+function getEffectOption(){
+	var e={
+		count:document.getElementById("count_selecter").value,
+		color:document.getElementById('color_selecter').value,
+		speed:document.getElementById("speed_selecter").value,
+		effect: document.getElementById("option1").selectedOptions[0].value + (document.getElementById("rand").checked?"rand":""),
+	};
+	if(document.getElementById("rand").checked){
+		e.x=document.getElementById("rand_x").value;
+		e.y=document.getElementById("rand_y").value;
+	}
+	return e;
+}
+
+//=============================== TTS 모듈 ===============================
+
+
+let voices = [],setVoiceList=()=>{
+	voices=window.speechSynthesis.getVoices();
+};
+setVoiceList();
+if (window.speechSynthesis.onvoiceschanged !== undefined){
+	window.speechSynthesis.onvoiceschanged=setVoiceList;
+}
+//TTS용
+//=============================== 채팅 모듈 ===============================
+var users=["nightbot","twipkr","bbangddeock","ssakdook"],users_lenght=users.length;
+var voices_user=[];
+
+function TTS_Chatuser_add(display_id){
+	commandSant.channel.getUser(display_id,function(t){
+		var u=JSON.parse(t)["users"][0];
+		voices_user.push([display_id,"<a onclick=''></a>"]);
+	})
+}
+function  TTS_Chatuser_remove(display_id){
+
+}
+
+
+function ChatConnect(oauth){// 사용자 채널에 연결
+	commandSant.channel=new chatClient({password:oauth});
+	commandSant.channel.isMood=true;//매니져 권한
+	commandSant.channel.onCommand=function(message,parsed){
+		if(commandSant.conn)
+			commandSant.conn.call(commandSant.channel,message,parsed);
+	};
+	commandSant.channel.onChating=function(parsed){//채팅 인식
+		commandSant.chats.push({"display-name":parsed["display-name"],"display-id":parsed["display-id"],"message":parsed["message"]})
+		if(commandSant.chat)
+			commandSant.chat.call(commandSant.channel,parsed);
+	};
+	commandSant.channel.getUser(commandSant.channel.username,function(t){
+		console.log(t);
+		commandSant.user=JSON.parse(t)["users"][0];
+		var ele =document.getElementById("login");
+		voices_user.push([commandSant.user["display-id"],"<a onclick=''>"+commandSant.user["display-name"]+"</a>"]);
+
+
+		users.push(commandSant.user["name"]);
+		users_lenght=users.length;
+
+		ele.innerHTML=commandSant.user["display_name"]+"님 반갑습니다";
+		ele.setAttribute("onclick","");
+	});
+	commandSant.channel.open();
+	//display-name
+}
+
+// 통신 데이터  - 사용자 _id
+//{"display_name":"네오캣짱","_id":"129955642","name":"neocats_","type":"user","bio":null,
+//"created_at":"2016-07-19T12:28:20.760139Z","updated_at":"2020-03-22T22:30:44.942731Z","logo":"로고"}
+
+//명령 채널
+function connectCommand(oauth){//명령 채널에 연결
+	const target="recodingbot";//명령 채
+	if(commandSant.command)return //commandSant.channel.close();//기존연결 해제
+	commandSant.command = new chatClient({channel:target,username:commandSant.channel.username,password:oauth});
+	/*
+	commandSant.command.isMood=true;//매니져 권한
+	commandSant.command.onCommand=function(comm,parsed){
+		//동일 채널 명령이거나
+		if(parsed["_id"] != commandSant.user['_id'])return;//채널명이 동일하지 않으면 제거
+		delete comm[0];
+		for(var i of commandSant.conn){
+			if(i.hasOwnProperty("onCommand"))
+				i.onCommand(comm,parsed);
+		}
+	}*/
+	commandSant.command.open();
+}
+
+function sandCommand(comm){
+	commandSant.command.onSend("#"+commandSant.user['_id']+" "+comm);
+}
+
+//=============================== 기본 모듈 ===============================
+function getParams(name, address = window.location.href) {
+	let url;
+	let results = "";
+	url = new URL(address);
+	if (typeof url.searchParams === 'undefined') {
+		results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(address);
+		if (results == null) {
+			return null;
+		} else {
+			return decodeURI(results[1]) || 0;
+		}
+	} else {
+		return url.searchParams.get(name);
+	}
+}
+
+//=============================== 페이지 모듈 ===============================
+function load_page(){
+	if(!location.hash||
+			["#home","#effect","#tts","#emote","#cromark","#timmer","#question","#programmer","#donate","#discord"].indexOf(location.hash)==-1)
+		location.hash="home";
+	// console.log("./page/"+location.hash.substring(1)+".html");
+
+	// document.getElementById("main").setAttribute("src","./page/"+location.hash.substring(1)+".html");
+	ajax("./page/"+location.hash.substring(1)+".html",function(txt){
+		let ele = document.getElementById("content");
+		ele.innerHTML=txt;
+		var scripts = ele.getElementsByTagName("script");
+		for(var i of scripts)
+			eval(i.innerHTML);
+		if(location.hash=="#effect"){
+			console.log(document.getElementById("option1"));
+			console.log(options);
+			document.getElementById("count_selecter").value=options.count;
+			document.getElementById('color_selecter').value=options.color||"#ffffff";
+			document.getElementById("speed_selecter").value=options.speed;
+
+		}
+	});
+}
+
+function ajax(url,f){//호출
+	var xmlhttp;
+	if(window.XMLHttpRequest)
+		xmlhttp=new XMLHttpRequest();
+	else xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			//item(JSON.parse(this.responseText),tap);
+			typeof f=='function'&&f(this.responseText);
+		}
+	};
+	xmlhttp.open("GET",url, true);
+	xmlhttp.send();
+}
+
+function copyToClipboard(value) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", value);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+	document.body.removeChild(aux);
+}
