33197342b30f1da98080b04172b76fa04f21b03c
Soung jin Ryu <neocat@kakao.com>
2020-03-30 18:58:13 +0900

    링크

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ window.addLink=document.addLink=function(a,b){b=document.head.C('link');b.src=a;
 window.addStyle=document.addStyle=function(a,b){b=document.head.C('style');b.innerHTML=a;return b};
 
 window.oauth_client_id = "g1rhyzp1s7y2d755xqjn1otspdgvc3";
-// window.oauth_redirect_uri = "https://patrickmonster.github.io/tgd/";
-window.oauth_redirect_uri = "http://549.ipdisk.co.kr/chatbot/";
+window.oauth_redirect_uri = "https://patrickmonster.github.io/tgd/";
+// window.oauth_redirect_uri = "http://549.ipdisk.co.kr/chatbot/";
 //채팅
 let commandSant = {
 	command:null,channel:null,user:{}, conn:0,chat:0,chats:[],
